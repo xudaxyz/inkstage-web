d0566e2ebff9bff2f10548ea05df84bb1d2797da
xuda <xuda@outlook.com>
2026-03-15 18:24:25 +0800

    移除换行符检查

--- eslint.config.js
+++ eslint.config.js
@@ -31,7 +31,6 @@ export default defineConfig([
         'exports': 'never',
         'functions': 'never'
       }],
-      'linebreak-style': ['error', 'windows'],
       'no-trailing-spaces': 'error',
       'eol-last': 'error',
       'object-curly-spacing': ['error', 'always'],
