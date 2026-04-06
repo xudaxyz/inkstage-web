89870a03dcfc03f0f2a790d36a3b6a3aa6db5ed7
xuda <xuda@outlook.com>
2026-04-06 12:18:09 +0800

    修改首页动画

--- src/pages/front/Home.tsx
+++ src/pages/front/Home.tsx
@@ -289,10 +289,10 @@ const Home: React.FC = () => {
               <AnimatePresence mode="wait">
                 <motion.div
                   key={`${selectedCategory}-${selectedTag}-${keyword}`}
-                  initial={{ opacity: 0, scale: 0.98, y: 10 }}
-                  animate={{ opacity: 1, scale: 1, y: 0 }}
-                  exit={{ opacity: 0, scale: 0.98, y: -10 }}
-                  transition={{ duration: 0.3, ease: 'easeOut' }}
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -10 }}
+                  transition={{ duration: 0.25, ease: 'easeInOut' }}
                   className="w-full"
                 >
                   <InfiniteScrollContainer
