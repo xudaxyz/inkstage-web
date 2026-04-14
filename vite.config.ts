fad6504cdb99cffcb8969620b16acf7183f8c713
xuda <xuda@outlook.com>
2026-04-14 11:29:39 +0800

    commit: - 签出分支，修改server路径

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://39.97.234.93:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace('/api', '')
       }
